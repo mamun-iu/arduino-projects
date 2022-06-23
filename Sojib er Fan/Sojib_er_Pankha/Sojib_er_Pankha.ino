efbf51a871998b17d566a16a17c615b55dfe3b7b
mamun-iu <mamun.iu15@gmail.com>
2022-06-23 13:45:03 +0600

    Sojib er pankha code added

---
+++ Sojib er Fan/Sojib_er_Pankha/Sojib_er_Pankha.ino
@@ -0,0 +1,68 @@
+#include "DHT.h"
+
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define DHTTYPE DHT11
+#define DHTPIN A0 
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+#define DHTTYPE DHT11   // DHT 11
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  lcd.begin();
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  /*Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));*/
+lcd.clear();
+lcd.print("TEMP: ");
+lcd.print(t); // display the temperature
+lcd.print("C ");
+lcd.setCursor(0,1); // move cursor to next line
+lcd.print("FAN: ");
+lcd.print(h); // display the fan speed
+lcd.print("%");
+delay(200);
+  
+
+}
